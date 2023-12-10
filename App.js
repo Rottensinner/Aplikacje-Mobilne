2f555ba8bba77717c256bfe3010d5e4145c48c53
flex01 <flex01@protonmail.com>
2023-12-11 00:56:13 +0100

    Added bottom tab navigation
    - Added screens folder with screen files: Home, Profile and Search
    - Added bottom tab navigation
    - removed StyleSheet and other unused imports from App.js file

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from 'react';
+import BottomTabNavigation from './navigation/BottomTabNavigation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,22 +29,14 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Bottom Navigation'
+          component={BottomTabNavigation}
+          options={{headerShown:false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textStyle: {
-    fontFamily: "regular",
-    fontSize: 20,
-  }
-});
