c34a960964dbccf75cb517fbdd57879f3c689c5a
flex01 <flex01@protonmail.com>
2023-12-11 00:03:26 +0100

    Added custom font
    Added Montserrat font-family to the project and loading custom font-family

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Montserrat-Regular.ttf"),
+    light: require("./assets/fonts/Montserrat-Light.ttf"),
+    bold: require("./assets/fonts/Montserrat-Bold.ttf"),
+    medium: require("./assets/fonts/Montserrat-Medium.ttf"),
+    semiBold: require("./assets/fonts/Montserrat-SemiBold.ttf"),
+    extrabold: require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+    
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  },[fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily: "regular",
+    fontSize: 20,
+  }
 });
