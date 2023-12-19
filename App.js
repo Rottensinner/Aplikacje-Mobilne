f479d254f846e493db5b580c3b2685d17f88995c
flex01 <flex01@protonmail.com>
2023-12-19 17:53:28 +0100

    Reformatted code in files and moved Stylesheet
    - Reformatted code in app.js and ProductRow component
    - Moved Stylesheet from ProductRow component into the productRow.style.js file

--- App.js
+++ App.js
@@ -1,48 +1,47 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useFonts } from 'expo-font';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from 'react';
-import BottomTabNavigation from './navigation/BottomTabNavigation';
-import { ProductDetails } from './screens';
+import { useCallback } from "react";
+import BottomTabNavigation from "./navigation/BottomTabNavigation";
+import { ProductDetails } from "./screens";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    regular: require("./assets/fonts/Montserrat-Regular.ttf"),
-    light: require("./assets/fonts/Montserrat-Light.ttf"),
-    bold: require("./assets/fonts/Montserrat-Bold.ttf"),
-    medium: require("./assets/fonts/Montserrat-Medium.ttf"),
-    semiBold: require("./assets/fonts/Montserrat-SemiBold.ttf"),
-    extrabold: require("./assets/fonts/Montserrat-ExtraBold.ttf"),
-  })
+    const [fontsLoaded] = useFonts({
+        regular: require("./assets/fonts/Montserrat-Regular.ttf"),
+        light: require("./assets/fonts/Montserrat-Light.ttf"),
+        bold: require("./assets/fonts/Montserrat-Bold.ttf"),
+        medium: require("./assets/fonts/Montserrat-Medium.ttf"),
+        semiBold: require("./assets/fonts/Montserrat-SemiBold.ttf"),
+        extrabold: require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+    });
 
-  const onLayoutRootView = useCallback(async() => {
-    
-    if(fontsLoaded) {
-      await SplashScreen.hideAsync();
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
     }
-  },[fontsLoaded]);
 
-  if(!fontsLoaded) {
-    return null;
-  }
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name='Bottom Navigation'
-          component={BottomTabNavigation}
-          options={{headerShown:false}}
-        />
-        <Stack.Screen 
-          name='ProductDetails'
-          component={ProductDetails}
-          options={{headerShown:false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Bottom Navigation"
+                    component={BottomTabNavigation}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="ProductDetails"
+                    component={ProductDetails}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -2,18 +2,19 @@ import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { SIZES } from '../../constants'
 import ProductCardView from './ProductCardView'
+import styles from './productRow.style'
 
 const ProductRow = () => {
     const products = [1, 2, 3, 4]
 
     return (
-        <View style={{marginTop: SIZES.medium}}>
+        <View style={styles.container}>
             <FlatList
-            data={products}
-            renderItem={({item}) => (<ProductCardView />)}
-            horizontal
-            contentContainerStyle={{columnGap: SIZES.medium}}
-        />
+                data={products}
+                renderItem={({item}) => (<ProductCardView />)}
+                horizontal
+                contentContainerStyle={{columnGap: SIZES.medium}}
+            />
         </View>
     )
 }
