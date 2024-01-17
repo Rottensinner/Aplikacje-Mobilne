f347bb02ed20ae0b7d9b8ba170eee545cff3b222
Kamil Bielecki <BieKam1C@gmail.com>
2024-01-17 03:12:50 +0100

    Dodanie styli dla komponentu Profile

---
+++ screens/profile.style.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../constants";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightWhite,
+  },
+  cover: {
+    width: "100%",
+    height: "60%",
+    resizeMode: "cover", 
+  },
+  profileContainer:{
+    flex: 1,
+    alignItems: "center"
+  },
+  profile:{
+    height: 155,
+    width: 155,
+    borderRadius: 999,
+    borderColor: COLORS.primary,
+    borderWidth: 2,
+    resizeMode: "cover",
+    marginTop: -280,
+  },
+  name: {
+    fontWeight: 'bold',
+    color: COLORS.primary,
+    marginVertical: 5,
+  },
+
+  loginBtn:{
+    backgroundColor: COLORS.secondary,
+    padding: 2,
+    borderWidth:0.4,
+    borderColor: COLORS.primary,
+    borderRadius: SIZES.xxLarge
+  },
+  menuText:{
+    fontFamily: "regular",
+    color: COLORS.gray,
+    marginLeft: 20,
+    fontWeight: '600',
+    fontSize: 14,
+    lineHeight: 26
+  },
+  menuWrapper: {
+    marginTop: SIZES.xLarge,
+    width: SIZES.width - SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    borderRadius: 12,
+  },
+
+  menuItem: (borderBottomWidth) => ({
+    borderBottomWidth: borderBottomWidth,
+    flexDirection: "row",
+    paddingHorizontal: 30, // Poprawiona literówka
+    paddingVertical: 15, // Poprawiona literówka
+    borderColor: COLORS.gray,
+  }),
+});
