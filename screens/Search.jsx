495e0ffe6696c88a576b9bdcd4df20771e0fc0f9
Rottensinner <aleksgabrys@gmail.co>
2024-01-18 22:19:26 +0100

    poprawa wyszukiwania

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,72 +1,65 @@
-import { View, Text, TouchableOpacity, TextInput,Image } from 'react-native'
-import React, {useState} from 'react';
-import styles from './search.style';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, FlatList, Image, SafeAreaView } from 'react-native';
 import { Feather, Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { COLORS, SIZES } from '../constants';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
-import { FlatList } from 'react-native';
 import SearchTitle from '../components/products/SearchTitle';
-
+import styles from './search.style';
+import { COLORS, SIZES } from '../constants';
 
 const Search = () => {
   const [searchKey, setSearchKey] = useState('');
-  const [searchResults, setSearchResults] = useState('');
-  console.log('Search Results:', searchResults);
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleInputChange = (text) => {
+    console.log("Wpisano:", text);
+    setSearchKey(text);
+  };
 
   const handleSearch = async () => {
+    console.log("jazda");
     try {
-      console.log('Search Key:', searchKey); // Log the search key before sending the request
-  
-      const response = await axios.get(`http://10.10.2.2:3000/api/products/search/${searchKey}`);
-      console.log('Response Data:', response.data);
+      const response = await axios.get(`http://10.0.2.2:3000/api/products/search/${searchKey}`);
+      console.log("Odpowiedź z serwera:", response.data);
       setSearchResults(response.data);
     } catch (error) {
-      console.log('Error:', error);
+      console.error("Błąd podczas wyszukiwania:", error);
     }
-  }
-
-  const handleSearchKeyChange = (text) => {
-    console.log('Search Key:', text);
-    setSearchKey(text);
-  }
+  };
 
   return (
     <SafeAreaView>
       <View style={styles.searchContainer}>
         <TouchableOpacity>
-          <Ionicons name="camera-outline" size={SIZES.xLarge} style={styles.searchIcon} />
+          <Ionicons name="camera-outline" size={SIZES.xLarge} style={styles.searchIcon}/>
         </TouchableOpacity>
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
             value={searchKey}
-            onChangeText={handleSearchKeyChange}
+            onChangeText={handleInputChange} // Zmieniono tutaj
             placeholder="Czego szukasz?"
           />
         </View>
         <View>
-          <TouchableOpacity style={styles.searchBtn} onPress={() => handleSearch()}>
+          <TouchableOpacity style={styles.searchBtn} onPress={handleSearch}>
             <Feather name="search" size={24} color={COLORS.offwhite} />
           </TouchableOpacity>
-          </View>
-      </View>
-      {searchResults.length === 0 ?(
-        <View style={{flex:1}}>
-          <Image source={require('../assets/5613.png')}
-          style={styles.searchImage}/>
         </View>
-      ):(
+      </View>
+      {searchResults.length === 0 ? (
+        <View style={{flex:1}}>
+          <Image source={require('../assets/5613.png')} style={styles.searchImage}/>
+        </View>
+      ) : (
         <FlatList
-        data={searchResults}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => <SearchTitle item={item} />}
-        style={{ marginHorizontal: 12 }}
-      />
+          data={searchResults}
+          keyExtractor={(item) => item._id}
+          renderItem={({item}) => <SearchTitle item={item}/>}
+          style={{marginHorizontal:12}}
+        />
       )}
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
